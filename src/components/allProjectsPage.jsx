25321d5ce7fa9a9c7146b436aba14fb002bac50f
Jonathan Faria <jonathanfdsn@gmail.com>
2023-03-30 20:49:18 -0300

    Mudanças layout e add estilos page Allprojects

--- src/components/allProjectsPage.jsx
+++ src/components/allProjectsPage.jsx
@@ -3,17 +3,21 @@ import Link from "next/link";
 export const AllProjectsPage = ({ projects }) => {
     return (
         <>
-            <h1> Projetos</h1>
-            {projects.map(p => {
-                return (
-                    <section key={p.id}>
-                        <Link href={`/projects/${p.id}`}>
-                            <h3>{p.name}</h3>
-                        </Link>
-                        <p>{p.short_description}</p>
-                    </section>
-                )
-            })}
+            <h2 className="title">Projetos</h2>
+            <div className="cards-container">
+                {projects.map(p => {
+                    return (
+                        <div className="card" key={p.id}>
+                                <h3>{p.name}</h3>
+                            <Link href={`/projects/${p.id}`}>
+                                <div className="card-layer">
+                                    {p.short_description}
+                                </div>
+                            </Link>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
