9724bd9cbbbc7413a02191645d62534228b24836
Jonathan Faria <jonathanfdsn@gmail.com>
2023-04-05 17:54:17 -0300

    Texto about da home page

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 
 export const HomePage = ({ perfil }) => {
+    const abilities = perfil.abilities.split(',')
+
     return (
         <div className="home-container">
             <div className="info-content">
                 <Image alt='perfil image' width={220} height={220} src='/placeholder.jpg' />
                 <h1>Oi, sou <span>{perfil.name}</span></h1>
-                <h3>{perfil.abilities}</h3>
+                <h3>{perfil.job}</h3>
                 <div className="social-links">
                     <a href={perfil.links.github} target="_blank">
                         <i className='bx bxl-github'></i>
@@ -23,6 +25,12 @@ export const HomePage = ({ perfil }) => {
             </div>
             <div className="about-content">
                 <p>{perfil.about}</p>
+                <h4>Experiência com:</h4>
+                <ul>
+                    {abilities.map(a => (
+                        <li key={a}>{a}</li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
