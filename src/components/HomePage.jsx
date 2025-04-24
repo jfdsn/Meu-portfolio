2d25c5203655bdadf09bed4673e5a0215416b503
Jonathan Faria <jonathanfdsn@gmail.com>
2025-04-24 14:05:11 -0300

    refactor: Update profile image and styles in HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,35 +7,40 @@ export const HomePage = ({ profile }) => {
 
     return (
         <>
-            <section id="about" className="hero-content">
-                <div className="info-content">
-                    <Image alt='perfil image' width={240} height={268} src='/foto-perfil.webp' />
-                    <h1>Oi, sou <span>{profile.name}</span></h1>
-                    <h3>{profile.title}</h3>
-                    <div className="social-links">
-                        <a href={profile.contact.links.github} target="_blank">
-                            <i className='bx bxl-github'></i>
-                        </a>
-                        <a href={profile.contact.links.linkedin} target="_blank">
-                            <i className='bx bxl-linkedin-square'></i>
-                        </a>
+            <section id="hero" className="hero-section">
+                    <div className="hero-content">
+                        <Image alt='perfil image' width={240} height={268} src='/foto-perfil.webp' />
+                        <h1>Oi, sou <span>{profile.name}</span></h1>
+                        <h3>{profile.title}</h3>
+                        <div className="social-links">
+                            <a href={profile.contact.links.github} target="_blank">
+                                <i className='bx bxl-github'></i>
+                            </a>
+                            <a href={profile.contact.links.linkedin} target="_blank">
+                                <i className='bx bxl-linkedin-square'></i>
+                            </a>
+                        </div>
+                        <div className="email">
+                            <a href="mailto:jonathanfdsn@gmail.com">
+                                <i className='bx bxs-envelope'>
+                                    {profile.contact.email}
+                                </i>
+                            </a>
+                        </div>
+                        <div className="download-cv">
+                            <a href="/CV-Jonathan.pdf" download>
+                                <i className='bx bxs-download'></i>
+                                Download CV
+                            </a>
+                        </div>
                     </div>
-                    <div className="email">
-                        <a href="mailto:jonathanfdsn@gmail.com">
-                            <i className='bx bxs-envelope'>
-                                {profile.contact.email}
-                            </i>
-                        </a>
+            </section>
+            <section id="about" className="about-section">
+                <h3>Sobre</h3>
+                <div className="container">
+                    <div className="about-content">
+                        {profile.about}
                     </div>
-                    <div className="download-cv">
-                        <a href="/CV-Jonathan.pdf" download>
-                            <i className='bx bxs-download'></i>
-                            Download CV
-                        </a>
-                    </div>
-                </div>
-                <div className="about-content">
-                    {profile.about}
                 </div>
             </section>
             <section id="skills" className="skills-content">
